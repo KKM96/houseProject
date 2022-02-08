aadffab35a3385f17ca1760a8ed1956caa9bf286
kyj <justdoit3718@naver.com>
2022-02-08 14:14:02 +0900

    TT

--- terms/views.py
+++ terms/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Question
+from django.core.paginator import Paginator
 
 # Create your views here.
 def terms(request):
@@ -9,6 +10,23 @@ def terms(request):
     """
     terms 목록 출력
     """
+    # 입력 파라미터
+    page = request.GET.get('page', '1')  # 페이지
+    kw = request.GET.get('kw', '')  # 검색어
+
+    # 조회
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list': question_list}
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |  # 제목검색
+            Q(content__icontains=kw) |  # 내용검색
+            Q(author__username__icontains=kw) |  # 질문 글쓴이검색
+            Q(answer__author__username__icontains=kw)  # 답변 글쓴이검색
+        ).distinct()
+
+    # 페이징처리
+    paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+
+    context = {'question_list': page_obj, 'page': page, 'kw': kw}
     return render(request, 'house/terms.html', context)
