5e88b5738ac20868d937f40c3fd3251cc4eb0d33
kkm <dyd2003@naver.com>
2022-02-09 17:18:40 +0900

    news

--- news/views.py
+++ news/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def news(request):
     return render(request, 'house/news.html')
+
+def news_naver(request):
+    return render(request, 'news/news_naver.html')
+
+def news_daum(request):
+    return render(request, 'news/news_daum.html')
+
+def htsale(request):
+    return render(request, 'news/htsale.html')
+
+def htmonth(request):
+    return render(request, 'news/htmonth.html')
+
+def htsub(request):
+    return render(request, 'news/htsub.html')
