70019a9050042d55f484110fb2a36c78c1b65b34
MingyeongKim0708 <kimmg0708@gmail.com>
2022-02-14 14:06:53 +0900

    community1

---
+++ community/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from community.models import Post, Answer
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['subject', 'content']
+        label = {
+            'subject' : '제목',
+            'content' : '내용'
+        }
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ['content']
+        label = {
+            'content' : '답변내용'
+        }
--- community/models.py
+++ community/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+#게시글
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=200)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.subject
+
+#답글
+class Answer(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
